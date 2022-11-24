a2f5587d70ec0e30163f201d7162d0143a965a26
Shinichiro Yabu <scin.ichi@gmail.com>
2022-11-24 11:05:07 +0900

    v1.0.0

---
+++ content.js
@@ -0,0 +1,48 @@
+const qs = (query, p) => (p || document).querySelector(query);
+const qsa = (query, p) => [].slice.call((p || document).querySelectorAll(query));
+
+const main = () => {
+	const channelName = (el) => el.getAttribute('title')
+
+	// スタイルを追加
+	const stylesheet = document.createElement('style')
+	document.body.appendChild(stylesheet)
+	const s = stylesheet.sheet
+	s.insertRule('#contents { display: flex; }')
+	s.insertRule('ytd-rich-grid-row { display: contents; }')
+	s.insertRule('ytd-rich-grid-row > #contents.ytd-rich-grid-row { display: contents; }')
+
+	// すべての登録チャンネルを表示
+	const expander = qsa('#expander-item')[1]
+	if (expander) {
+		expander.click()
+	}
+
+	// 登録チャンネル名のリスト
+	const subscriptions = qsa('#items #endpoint[href^="/c/"], #items #endpoint[href^="/@"]')
+		.map(channelName)
+
+	const run = () => {
+		qsa('ytd-rich-grid-media').forEach((el) => {
+			const ch = channelName(qs('#avatar-link', el))
+			el.parentNode.parentNode.style.display = subscriptions.includes(ch) ? 'block' : 'none'
+		})
+	}
+
+	const observer = new MutationObserver(run)
+	observer.observe(qs('#contents'), {
+		childList: true
+	})
+
+	run()
+}
+
+const loader = () => {
+	if (!qs('#contents') || !qs('#items #endpoint[href^="/c/"], #items #endpoint[href^="/@"]')) {
+		setTimeout(loader, 100)
+		return
+	}
+	main()
+}
+
+loader()
